1760b4cbb7d469f9125f66645b67bad37a984b45
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-01-01 09:19:02 +0300

    Flip values

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import './index.css';
 function Square(props)  {
  
     return (
-      <button 
-        className="square" 
-        onClick={props.onClick()}
-        >
+      <button className="square" onClick={props.onClick} >
         {props.value}
       </button>
     );
@@ -18,14 +15,18 @@ class Board extends React.Component {
   constructor(props){
     super(props) 
     this.state = {
-     squares: Array(9).fill(null)
+     squares: Array(9).fill(null),
+     xIsNext: true,
     }
   }
 
   handleClick(i) {
    const squares = this.state.squares.slice();
-   squares[i] = 'x';
-   this.setState({squares: squares});
+   squares[i] = this.state.xIsNext ? 'x' : 'o';
+   this.setState({
+    squares: squares,
+    xIsNext: !this.state.xIsNext,
+  });
   }
   renderSquare(i) {
     return <Square value={this.state.squares[i]}
@@ -33,7 +34,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
 
     return (
       <div>
